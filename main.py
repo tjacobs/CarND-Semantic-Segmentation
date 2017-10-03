a0f6138aa5fc50c9e5a3cb970d0625bbd6157b55
Tom Jacobs <tjacobs@expensify.com>
2017-10-03 13:45:20 +1100

    Train that loss

--- main.py
+++ main.py
@@ -78,8 +78,17 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :param num_classes: Number of classes to classify
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
-    # TODO: Implement function
-    return None, None, None
+
+    # Flatten logits
+    logits = tf.reshape(nn_last_layer, (-1, num_classes))
+
+    # Get loss
+    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=correct_label))
+
+    # Train this baby
+    train_op = tf.train.AdamOptimizer( 1e-4).minimize(cross_entropy_loss)
+
+    return logits, train_op, cross_entropy_loss
 tests.test_optimize(optimize)
 
 
